26b7ef553ad3258b97143ca1d95e9e76af2b6f1e
Tosturi <allexgodofwar@gmail.com>
2026-04-18 21:48:32 +0500

    Miration to the new MC version (26.1.2)

--- src/main/java/com/tosturi/testmod/item/ModItems.java
+++ src/main/java/com/tosturi/testmod/item/ModItems.java
@@ -2,7 +2,6 @@ package com.tosturi.testmod.item;
 
 import com.tosturi.testmod.TestMod;
 import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -12,10 +11,6 @@ public class ModItems {
 
     public static final DeferredItem<Item> FLORICS = ITEMS.registerItem(
             "florics",
-            (Item.Properties props) -> new Item(props.stacksTo(64))
+            props -> new Item(props.stacksTo(64))
     );
-
-//    public static void register(IEventBus eventBus) {
-//        ITEMS.register(eventBus);
-//    }
 }
